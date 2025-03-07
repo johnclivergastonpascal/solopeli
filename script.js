51801742595e4e8983e87a8e6e01d5f2b60f1d55
GP <140924052+johnclivergastonpascal@users.noreply.github.com>
2025-03-07 07:03:32 -0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const menu = document.querySelector(".menu");
 hamburger.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
+
+function searchMovies() {
+    const searchInput = document.getElementById("searchInput").value.toLowerCase(); // Obtener el valor del input
+    const gridItems = document.querySelectorAll(".grid-item"); // Seleccionar todas las secciones del grid
+
+    gridItems.forEach(item => {
+        const title = item.querySelector("h3").textContent.toLowerCase(); // Obtener el título de la película
+        if (title.includes(searchInput)) {
+            item.style.display = ""; // Mostrar la sección si coincide
+        } else {
+            item.style.display = "none"; // Ocultar la sección si no coincide
+        }
+    });
+}
